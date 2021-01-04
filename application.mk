5d0e50d9f7e4ebb2e4314fd5a64b9feffd7ad38f
Joshua Wong <68436775+joshua-nmi@users.noreply.github.com>
2021-01-04 12:55:16 -0500

    Initial commit

---
+++ application.mk
@@ -0,0 +1,53 @@
+#******************************************************************************
+#
+# Step 1
+# Define the locations of the various SDKs and libraries.
+#
+#******************************************************************************
+NM_SDK    ?= $(shell pwd)/../nmsdk
+AMBIQ_SDK ?= $(shell pwd)/../AmbiqSuite-R2.5.1
+FREERTOS  ?= $(shell pwd)/../FreeRTOS/FreeRTOS
+CORDIO    ?= $(shell pwd)/../AmbiqSuite-R2.5.1/third_party/cordio
+UECC      ?= $(shell pwd)/../AmbiqSuite-R2.5.1/third_party/uecc
+LORAMAC   ?= $(shell pwd)/../LoRaMac-node
+
+#******************************************************************************
+#
+# Step 2
+# Specify the location of the board support package to be used.
+#
+#******************************************************************************
+BSP_DIR := $(NM_SDK)/bsp/nm180100evb
+
+#******************************************************************************
+#
+# Step 3
+# Specify output target name
+#
+#******************************************************************************
+ifdef DEBUG
+    TARGET   := nmapp-dev
+else
+    TARGET   := nmapp
+endif
+
+#******************************************************************************
+#
+# Step 4
+# Include additional source, header, libraries or paths below.
+#
+# Examples:
+#   INCLUDES += -Iadditional_include_path
+#   VPATH    += additional_source_path
+#   LIBS     += -ladditional_library
+#******************************************************************************
+
+INCLUDES += -I$(NM_SDK)/platform/console
+
+VPATH += $(NM_SDK)/platform/console
+
+SRC += console_task.c
+SRC += gpio_service.c
+SRC += iom_service.c
+
+SRC += application.c
